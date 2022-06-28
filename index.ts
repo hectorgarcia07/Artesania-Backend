9a9cbb8688b98097b0e9fff388abfe6af346e2a0
hectorgarcia07 <csharp12321@gmail.com>
2022-06-28 13:07:41 -0500

    Heroku

--- index.ts
+++ index.ts
@@ -35,6 +35,10 @@ mongoose.connect(url)
     console.log('error connecting to MongoDB:', error.message);
   });
 
+app.get('/', function (_req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.use('/api/users', UserRouter);
 app.use('/api/shoes', ShoesRouter);
 app.use(middleware.errorHandling);
